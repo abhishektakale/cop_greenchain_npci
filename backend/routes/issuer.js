5081f16e30cf6041a226d09fa73b1e5d9cd8e3c1
Harsh Gupta <harsh.gupta@npci.org.in>
2023-10-08 16:14:33 +0530

    Added backend integration with blockchain

--- backend/routes/issuer.js
+++ backend/routes/issuer.js
@@ -1,13 +1,12 @@
 var express = require('express');
+var Issuer = require('../services/issuer');
 // var token = require('../schemas/token');
 var router = express.Router();
 
 router.post('/createToken', function (req, res, next) {
     const issuerService = new Issuer();
     const tokenDetails = req.body;
-    // const isValid = token.CreateTokenSchema.validate(tokenDetails);
-
-    res.send(issuerService.issueToken(tokenDetails));
+    res.send(issuerService.createToken(tokenDetails));
 });
 
 router.post('/issueToken', function (req, res, next) {
@@ -16,9 +15,5 @@ router.post('/issueToken', function (req, res, next) {
 
     res.send(issuerService.issueToken(tokenDetails));
 });
-
-// router.post('/issueToken', function (req, res, next) {
-//   res.send('respond with a resource');
-// });
 
 module.exports = router;
